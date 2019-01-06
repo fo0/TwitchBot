2d3edda5833707b7d114446905853b6839ec1b0d
fo0 <fo0@ubuntu>
2019-01-07 00:22:03 +0100

    added controller to main

--- TwitchBot/src/main/java/com/fo0/twitchbot/main/Main.java
+++ TwitchBot/src/main/java/com/fo0/twitchbot/main/Main.java
@@ -1,9 +1,11 @@
 package com.fo0.twitchbot.main;
 
+import com.fo0.downloader.controller.Controller;
+
 public class Main {
 
 	public static void main(String[] args) {
-
+		Controller.bootstrap(args);
 	}
 
 }
